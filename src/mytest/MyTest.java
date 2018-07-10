c2e99af8ed00b229a32115233a05e6c4d567b3d2
Linbee <402515715@qq.com>
2018-07-11 00:39:54 +0800

    Initial commit

---
+++ src/mytest/MyTest.java
@@ -0,0 +1,35 @@
+package mytest;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import pojo.Employee;
+
+public class MyTest {
+
+	/*
+	 * 根据xml配文件，创建一个SqlSessionFactory对象
+	*/
+	@Test
+	public void start() throws IOException
+	{
+		String resource ="mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSession=new SqlSessionFactoryBuilder().build(inputStream);
+	
+		SqlSession openSession = sqlSession.openSession();
+	
+		Employee emp=openSession.selectOne("selectEmp",1);
+		
+		openSession.close();
+		
+		System.out.println(emp);
+		
+	}
+}
